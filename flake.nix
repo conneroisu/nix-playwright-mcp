decf2b05d5f9c3fb2e302a21d1ee4696cae5046b
connerohnesorge <conneroisu@outlook.com>
2025-08-26 06:04:10 -0500

    0.35.0

--- flake.nix
+++ flake.nix
@@ -1,40 +1,37 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      eachSystem =
-        f:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+  outputs = {nixpkgs, ...}: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
 
-      inherit (nixpkgs) lib;
+    inherit (nixpkgs) lib;
 
-      npmDepsHash = lib.trim (builtins.readFile ./npm-hash.txt);
-    in
-    {
-      packages = eachSystem (pkgs: {
-        default = pkgs.callPackage ./mcp-server.nix {
-          inherit npmDepsHash;
-        };
-      });
+    npmDepsHash = lib.trim (builtins.readFile ./npm-hash.txt);
+  in {
+    packages = eachSystem (pkgs: {
+      default = pkgs.callPackage ./mcp-server.nix {
+        inherit npmDepsHash;
+      };
+    });
 
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = [
-            pkgs.nixd
-            pkgs.alejandra
-            pkgs.just
-          ];
-        };
-        # A Nix shell for running the update script.
-        #
-        # Example: `nix develop .#ci -c ./update.bash`
-        ci = pkgs.mkShell {
-          packages = [
-            pkgs.prefetch-npm-deps
-          ];
-        };
-      });
-    };
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.nixd
+          pkgs.alejandra
+          pkgs.just
+          pkgs.prefetch-npm-deps
+        ];
+      };
+      # A Nix shell for running the update script.
+      #
+      # Example: `nix develop .#ci -c ./update.bash`
+      ci = pkgs.mkShell {
+        packages = [
+          pkgs.prefetch-npm-deps
+        ];
+      };
+    });
+  };
 }
